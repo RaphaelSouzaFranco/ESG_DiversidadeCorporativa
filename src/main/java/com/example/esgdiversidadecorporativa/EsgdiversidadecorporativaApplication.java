a1b2e1c6bc67f213b05fe00c7f22d6f4b498cce1
RaphaelSouzaFranco <raphchio@gmail.com>
2025-11-01 16:41:07 -0300

    first commit

---
+++ src/main/java/com/example/esgdiversidadecorporativa/EsgdiversidadecorporativaApplication.java
@@ -0,0 +1,13 @@
+package com.example.esgdiversidadecorporativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgdiversidadecorporativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgdiversidadecorporativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esgdiversidadecorporativa/EsgdiversidadecorporativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esgdiversidadecorporativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgdiversidadecorporativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
